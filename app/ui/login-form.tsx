932c29d9741f9f519829dd26e447a12c8e4c78fb
fitogabo <gapantoj@uc.cl>
2024-03-07 01:12:36 -0300

    Update next-auth version and add Google login button

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -10,6 +10,7 @@ import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
 import { useFormState, useFormStatus } from 'react-dom';
 import { authenticate } from '@/app/lib/actions';
+import { signIn } from 'next-auth/client';
 
 export default function LoginForm() {
   const [code, action] = useFormState(authenticate, undefined);
@@ -61,6 +62,7 @@ export default function LoginForm() {
           </div>
         </div>
         <LoginButton />
+        <GoogleLoginButton />
         <div className="flex h-8 items-end space-x-1">
           {code === 'CredentialSignin' && (
             <>
@@ -78,10 +80,20 @@ export default function LoginForm() {
 
 function LoginButton() {
   const { pending } = useFormStatus();
- 
+
   return (
     <Button className="mt-4 w-full" aria-disabled={pending}>
       Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+    </Button>
+  );
+}
+
+function GoogleLoginButton() {
+  return (
+    <Button
+      className="mt-4 w-full bg-red-600 text-white"
+      onClick={() => signIn('google')}>
+      Log in with Google
     </Button>
   );
 }
