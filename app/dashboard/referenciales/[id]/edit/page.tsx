983c59b9a25328a14cf7760bfc3a1b03f1b96285
fitogabo <gapantoj@uc.cl>
2024-03-09 01:47:45 -0300

    eliminado la llamada a fetchCustomers y la referencia a CustomerField. También he eliminado la propiedad customers del componente Form

--- app/dashboard/referenciales/[id]/edit/page.tsx
+++ app/dashboard/referenciales/[id]/edit/page.tsx
@@ -1,16 +1,13 @@
 import Form from '@/app/ui/referenciales/edit-form';
 import Breadcrumbs from '@/app/ui/referenciales/breadcrumbs';
-import { fetchCustomers, fetchReferencialById } from '@/app/lib/data';
-import { CustomerField, ReferencialForm } from '@/app/lib/definitions';
+import { fetchReferencialById } from '@/app/lib/data';
+import { ReferencialForm } from '@/app/lib/definitions';
 import { notFound } from 'next/navigation';
 
 export default async function Page({ params }: { params: { id: string } }) {
 
     const id = params.id;
-    const [referencial, customers]: [ReferencialForm | undefined, CustomerField[] | undefined] = await Promise.all([
-        fetchReferencialById(id),
-        fetchCustomers(),
-    ]);
+    const referencial: ReferencialForm | undefined = await fetchReferencialById(id);
 
     if (!referencial) {
         notFound();
@@ -28,7 +25,7 @@ export default async function Page({ params }: { params: { id: string } }) {
                     },
                 ]}
             />
-            <Form referencial={referencial!} customers={customers} />
+            <Form referencial={referencial!} />
         </main>
     );
 }
