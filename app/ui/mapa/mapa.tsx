60f0bf65d91421e98141a30d1c7e2fc9d5ae15c0
fitogabo <gapantoj@uc.cl>
2024-03-14 00:59:04 -0300

    Update map library and refactor map component

--- app/ui/mapa/mapa.tsx
+++ app/ui/mapa/mapa.tsx
@@ -1,39 +1,48 @@
 // mapa.tsx
-import { useEffect } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
+import { useEffect, useState } from 'react';
+import ReactMapGL, { Marker, Popup } from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
-const Mapa = ({ referenciales }) => {
-    useEffect(() => {
-        // Corrige el problema con los marcadores en Leaflet con Webpack
-        delete L.Icon.Default.prototype._getIconUrl;
-        L.Icon.Default.mergeOptions({
-            iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-            iconUrl: require('leaflet/dist/images/marker-icon.png'),
-            shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-        });
-    }, []);
+interface Referencial {
+    id: number;
+    lat: number;
+    lng: number;
+    name: string;
+}
+
+interface MapaProps {
+    referenciales: Referencial[];
+}
+
+const Mapa = ({ referenciales }: MapaProps) => {
+    const [viewport, setViewport] = useState({
+        latitude: 51.505,
+        longitude: -0.09,
+        zoom: 13,
+        width: "100%",
+        height: "100vh",
+    });
 
     return (
-        <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "100vh", width: "100%" }}>
-            <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            />
+        <ReactMapGL
+            {...viewport}
+            mapboxApiAccessToken={process.env.MAPBOX_TOKEN}
+            onViewportChange={(viewport) => setViewport(viewport)}
+        >
             {referenciales.map((referencial) => (
-                <Marker key={referencial.id} position={[referencial.lat, referencial.lng]}>
-                    <Popup>
+                <Marker key={referencial.id} latitude={referencial.lat} longitude={referencial.lng}>
+                    <Popup latitude={referencial.lat} longitude={referencial.lng} closeButton={true} closeOnClick={false}>
                         {referencial.name}
                     </Popup>
                 </Marker>
             ))}
-        </MapContainer>
+        </ReactMapGL>
     );
 };
 
 export async function getServerSideProps() {
     // Aquí debes reemplazar con tu lógica para obtener los datos de la base de datos
-    const referenciales = await fetchFromDatabase();
+    const referenciales: Referencial[] = await fetchFromDatabase();
 
     return {
         props: {
