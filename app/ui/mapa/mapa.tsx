ba6de5335b7c27d0fb4a647944007bfbc85f509d
fitogabo <gapantoj@uc.cl>
2024-03-18 17:19:44 -0300

    Update Mapa component to use Leaflet for rendering the map

--- app/ui/mapa/mapa.tsx
+++ app/ui/mapa/mapa.tsx
@@ -1,22 +1,17 @@
-import { useState } from 'react';
-import ReactMapGL, { ViewState } from 'react-map-gl';
+import { useEffect, useRef } from 'react';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
 const Mapa = () => {
-    const [viewport, setViewport] = useState<ViewState>({
-        latitude: 51.505,
-        longitude: -0.09,
-        zoom: 13,
-        width: "100vw",
-        height: "100vh",
-    });
+    const mapRef = useRef(null);
 
-    return (
-        <ReactMapGL
-            {...viewport}
-            mapboxApiAccessToken={process.env.MAPBOX_TOKEN}
-            onViewportChange={(nextViewport: ViewState) => setViewport(nextViewport)}
-        />
-    );
+    useEffect(() => {
+        if (mapRef.current) {
+            L.map(mapRef.current).setView([51.505, -0.09], 13);
+        }
+    }, []);
+
+    return <div id="map" ref={mapRef} style={{ height: "100vh", width: "100vw" }} />;
 };
 
 export default Mapa;
