df04da4a75983c10333015ce26238c8811bc37b7
fitogabo <gapantoj@uc.cl>
2024-03-20 00:15:13 -0300

    Refactor edit-form.tsx to improve code readability

--- app/ui/referenciales/edit-form.tsx
+++ app/ui/referenciales/edit-form.tsx
@@ -1,23 +1,16 @@
 'use client';
-
-import { colaboradores, referenciales } from '@prisma/client';
-import {
-  CheckIcon,
-  ClockIcon,
-  CurrencyDollarIcon,
-  UserCircleIcon,
-} from '@heroicons/react/24/outline';
+import { referenciales } from '@prisma/client';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
 import { updateReferencial } from '@/app/lib/actions';
 import { useFormState } from 'react-dom';
+import { UserCircleIcon, CurrencyDollarIcon } from '@heroicons/react/24/outline';
 
 type FormState = {
   message: string | null;
   errors: {
     colaboradorId?: string[];
     amount?: string[];
-    status?: string[];
   };
 };
 
@@ -26,7 +19,7 @@ export default function EditReferencialForm({
   colaborador,
 }: {
   referencial: referenciales;
-  colaborador: [];
+  colaborador: any[];
 }) {
   const initialState = { message: null, errors: {} };
   const updateReferencialWithId = updateReferencial.bind(null, referencial.id);
@@ -50,8 +43,8 @@ export default function EditReferencialForm({
               <option value="" disabled>
                 Select a colaborador
               </option>
-              {colaborador.map((colaborador) => (
-                <option key={colaboradores.id} value={colaboradores.id}>
+              {colaborador.map((colaborador: any) => (
+                <option key={colaborador.id} value={colaborador.id}>
                   {colaborador.name}
                 </option>
               ))}
@@ -82,7 +75,7 @@ export default function EditReferencialForm({
                 id="amount"
                 name="amount"
                 type="number"
-                defaultValue={referencial.amount}
+                defaultValue={referencial.monto}
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
                 aria-describedby="amount-error"
@@ -103,78 +96,16 @@ export default function EditReferencialForm({
           ) : null}
         </div>
 
-        {/* Referencial Status */}
-        <fieldset>
-          <legend className="mb-2 block text-sm font-medium">
-            Set the referencial status
-          </legend>
-          <div className="rounded-md border border-gray-200 bg-white px-[14px] py-3">
-            <div className="flex gap-4">
-              <div className="flex items-center">
-                <input
-                  id="pending"
-                  name="status"
-                  type="radio"
-                  value="pending"
-                  defaultChecked={referencial.status === 'pending'}
-                  className="h-4 w-4 border-gray-300 bg-gray-100 text-gray-600 focus:ring-2 focus:ring-gray-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-gray-600"
-                  aria-describedby="status-error"
-                />
-                <label
-                  htmlFor="pending"
-                  className="ml-2 flex items-center gap-1.5 rounded-full bg-gray-100 px-3 py-1.5 text-xs font-medium text-gray-600 dark:text-gray-300"
-                >
-                  Pending <ClockIcon className="h-4 w-4" />
-                </label>
-              </div>
-              <div className="flex items-center">
-                <input
-                  id="paid"
-                  name="status"
-                  type="radio"
-                  value="paid"
-                  defaultChecked={referencial.status === 'paid'}
-                  className="h-4 w-4 border-gray-300 bg-gray-100 text-gray-600 focus:ring-2 focus:ring-gray-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-gray-600"
-                />
-                <label
-                  htmlFor="paid"
-                  className="ml-2 flex items-center gap-1.5 rounded-full bg-green-500 px-3 py-1.5 text-xs font-medium text-white dark:text-gray-300"
-                >
-                  Paid <CheckIcon className="h-4 w-4" />
-                </label>
-              </div>
-            </div>
-          </div>
-        </fieldset>
-        {state.errors?.status ? (
-          <div
-            id="status-error"
-            aria-live="polite"
-            className="mt-2 text-sm text-red-500"
+
+        <div className="mt-6 flex justify-end gap-4">
+          <Link
+            href="/dashboard/referenciales"
+            className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
           >
-            {state.errors.status.map((error: string) => (
-              <p key={error}>{error}</p>
-            ))}
-          </div>
-        ) : null}
-        {state.message ? (
-          <div
-            id="message-error"
-            aria-live="polite"
-            className="mt-2 text-sm text-red-500"
-          >
-            <p key={state.message}>{state.message}</p>
-          </div>
-        ) : null}
-      </div>
-      <div className="mt-6 flex justify-end gap-4">
-        <Link
-          href="/dashboard/referenciales"
-          className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
-        >
-          Cancel
-        </Link>
-        <Button type="submit">Edit Referencial</Button>
+            Cancel
+          </Link>
+          <Button type="submit">Edit Referencial</Button>
+        </div>
       </div>
     </form>
   );
