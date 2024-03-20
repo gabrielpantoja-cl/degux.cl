68b32a1885744f7d68872cfb6eb9e70fb5344aeb
fitogabo <gapantoj@uc.cl>
2024-03-20 00:20:51 -0300

    Update metadata author and title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import { Metadata } from 'next';
- 
+
 export const metadata: Metadata = {
   title: {
-    template: '%s | Acme Dashboard | @lllariogonzalez',
-    default: 'Acme Dashboard by @lllariogonzalez',
+    template: '%s | Acme Dashboard | @fitogabo',
+    default: 'Acme Dashboard by @fitogabo',
   },
   description: 'The official Next.js Course Dashboard, built with App Router.',
   metadataBase: new URL('https://nextjs-dashboard-acme.vercel.app'),
-  authors: [{name: 'Mario Gonzalez', url:'https://www.linkedin.com/in/lllariogonzalez'}],
+  authors: [{ name: 'Gabriel Pantoja', url: 'https://www.linkedin.com/in/gapantoj/' }],
   keywords: ['Next.js 14', 'Acme', 'Dashboard', 'nextjs.org/learn', 'Server Actions'],
   openGraph: {
     title: 'Acme Dashboard',
@@ -19,9 +19,9 @@ export const metadata: Metadata = {
   },
   twitter: {
     site: '@acme',
-    description:'The official Next.js Learn Dashboard built with App Router.',
-    title:'Acme Dashboard by @lllariogonzalez',
-    creator:'@lllariogonzalez',
+    description: 'The official Next.js Learn Dashboard built with App Router.',
+    title: 'Acme Dashboard by @fitogabo',
+    creator: '@fitogabo',
   },
 };
 
