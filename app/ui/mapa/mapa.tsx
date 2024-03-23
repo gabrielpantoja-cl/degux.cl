7127fa9e06cd7e3c1488040b4e3368d0ea810ae8
fitogabo <gapantoj@uc.cl>
2024-03-23 18:23:15 -0300

    Refactor map component to use react-leaflet

--- app/ui/mapa/mapa.tsx
+++ app/ui/mapa/mapa.tsx
@@ -1,25 +1,17 @@
 // app/ui/mapa/mapa.tsx
 'use client';
-
-import { useEffect, useRef } from 'react';
-import L from 'leaflet';
+import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
 const Mapa = () => {
-    const mapRef = useRef(null);
-
-    useEffect(() => {
-        if (mapRef.current) {
-            const map = L.map(mapRef.current).setView([51.505, -0.09], 13);
-
-            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-                maxZoom: 19,
-            }).addTo(map);
-        }
-    }, []);
-
-    return <div id="map" ref={mapRef} style={{ height: "100vh", width: "100vw" }} />;
+    return (
+        <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "100vh", width: "100vw" }}>
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
+        </MapContainer>
+    );
 };
 
 export default Mapa;
