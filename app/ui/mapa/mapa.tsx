31385cecc74449df38822e9870952cb289d24e15
fitogabo <gapantoj@uc.cl>
2024-03-25 10:29:50 -0300

    Add data fetching and rendering of CircleMarkers on the map

--- app/ui/mapa/mapa.tsx
+++ app/ui/mapa/mapa.tsx
@@ -1,15 +1,44 @@
 // app/ui/mapa/mapa.tsx
 'use client';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import React, { useEffect, useState } from 'react';
+import { MapContainer, TileLayer, CircleMarker } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import axios from 'axios';
+
+// Define el tipo para los puntos
+type Point = {
+    lat: number;
+    lng: number;
+};
 
 const Mapa = () => {
+    // Usa la anotación de tipo con useState
+    const [data, setData] = useState<Point[]>([]);
+
+    useEffect(() => {
+        // Reemplaza la URL con la URL de tu base de datos
+        axios.get('https://api.vercel.com/postgres-database-url')
+            .then(response => {
+                setData(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching data: ', error);
+            });
+    }, []);
+
     return (
         <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "100vh", width: "100vw" }}>
             <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             />
+            {data.map((point, index) => (
+                <CircleMarker
+                    key={index}
+                    center={[point.lat, point.lng]}
+                    radius={20}
+                />
+            ))}
         </MapContainer>
     );
 };
