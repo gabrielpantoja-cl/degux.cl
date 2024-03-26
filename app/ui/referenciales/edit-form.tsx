b7b07767fbb028ccabd9c78605be689949c54961
fitogabo <gapantoj@uc.cl>
2024-03-25 21:56:52 -0300

    Update user authentication logic

--- app/ui/referenciales/edit-form.tsx
+++ app/ui/referenciales/edit-form.tsx
@@ -1,76 +1,52 @@
 // app/ui/referenciales/edit-form.tsx
 'use client';
 
-import { referenciales } from '@prisma/client';
+import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
+import {
+  CheckIcon,
+  ClockIcon,
+  CurrencyDollarIcon,
+  UserCircleIcon,
+} from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
 import { updateReferencial } from '@/app/lib/actions';
-import { UserCircleIcon, CurrencyDollarIcon } from '@heroicons/react/24/outline';
-import { useState } from 'react';
+import { useFormState } from 'react-dom';
 
-type FormState = {
-  message: string | null;
-  errors: {
-    colaboradorId?: string[];
-    monto?: string[];
-  };
-};
-
-function useCustomFormState(
-  action: (state: FormState) => Promise<FormState>,
-  initialState: FormState
-) {
-  const [state, setState] = useState<FormState>(initialState);
-
-  const dispatch = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-    const formState: FormState = {
-      message: formData.get('message') as string,
-      errors: {
-        colaboradorId: formData.getAll('colaboradorId') as string[],
-        monto: formData.getAll('monto') as string[],
-      },
-    };
-    const newState = await action(formState);
-    setState(newState);
-  };
-
-  return [state, dispatch] as const;
-}
-
-export default function EditReferencialForm({
-  referencial,
-  colaborador,
+export default function EditInvoiceForm({
+  invoice,
+  customers,
 }: {
-  referencial: referenciales;
-  colaborador: any[];
+  invoice: InvoiceForm;
+  customers: CustomerField[];
 }) {
+  // this not necessary, and invoice.id is the argument need
   const initialState = { message: null, errors: {} };
-  const updateReferencialWithId = updateReferencial.bind(null, referencial.id);
-  const [state, dispatch] = useCustomFormState(updateReferencialWithId, initialState)
+  const updateInvoiceWithId = updateReferencial.bind(null, invoice.id);
+  const [state, dispatch] = useFormState(updateReferencialWithId, initialState)
 
   return (
-    <form onSubmit={dispatch}>
+    <form action={dispatch}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
+        {/* Customer Name */}
         <div className="mb-4">
-          <label htmlFor="colaborador" className="mb-2 block text-sm font-medium">
-            Choose colaborador
+          <label htmlFor="customer" className="mb-2 block text-sm font-medium">
+            Choose customer
           </label>
           <div className="relative">
             <select
-              id="colaborador"
-              name="colaboradorId"
+              id="customer"
+              name="customerId"
               className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-              defaultValue={referencial.colaborador_id}
-              aria-describedby="colaborador-error"
+              defaultValue={invoice.customer_id}
+              aria-describedby="customer-error"
             >
               <option value="" disabled>
-                Select a colaborador
+                Select a customer
               </option>
-              {colaborador.map((colaborador: any) => (
-                <option key={colaborador.id} value={colaborador.id}>
-                  {colaborador.name}
+              {customers.map((customer) => (
+                <option key={customer.id} value={customer.id}>
+                  {customer.name}
                 </option>
               ))}
             </select>
@@ -78,7 +54,7 @@ export default function EditReferencialForm({
           </div>
           {state.errors?.colaboradorId ? (
             <div
-              id="colaborador-error"
+              id="customer-error"
               aria-live="polite"
               className="mt-2 text-sm text-red-500"
             >
@@ -89,28 +65,28 @@ export default function EditReferencialForm({
           ) : null}
         </div>
 
-        {/* Referencial Monto */}
+        {/* Invoice Amount */}
         <div className="mb-4">
-          <label htmlFor="monto" className="mb-2 block text-sm font-medium">
-            Elegir monto
+          <label htmlFor="amount" className="mb-2 block text-sm font-medium">
+            Choose an amount
           </label>
           <div className="relative mt-2 rounded-md">
             <div className="relative">
               <input
-                id="monto"
-                name="monto"
+                id="amount"
+                name="amount"
                 type="number"
-                defaultValue={referencial.monto}
-                placeholder="Enter CLP monto"
+                defaultValue={invoice.amount}
+                placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-                aria-describedby="monto-error"
+                aria-describedby="amount-error"
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
           {state.errors?.monto ? (
             <div
-              id="colaborador-error"
+              id="customer-error"
               aria-live="polite"
               className="mt-2 text-sm text-red-500"
             >
@@ -121,16 +97,78 @@ export default function EditReferencialForm({
           ) : null}
         </div>
 
-
-        <div className="mt-6 flex justify-end gap-4">
-          <Link
-            href="/dashboard/referenciales"
-            className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
+        {/* Invoice Status */}
+        <fieldset>
+          <legend className="mb-2 block text-sm font-medium">
+            Set the invoice status
+          </legend>
+          <div className="rounded-md border border-gray-200 bg-white px-[14px] py-3">
+            <div className="flex gap-4">
+              <div className="flex items-center">
+                <input
+                  id="pending"
+                  name="status"
+                  type="radio"
+                  value="pending"
+                  defaultChecked={invoice.status === 'pending'}
+                  className="h-4 w-4 border-gray-300 bg-gray-100 text-gray-600 focus:ring-2 focus:ring-gray-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-gray-600"
+                  aria-describedby="status-error"
+                />
+                <label
+                  htmlFor="pending"
+                  className="ml-2 flex items-center gap-1.5 rounded-full bg-gray-100 px-3 py-1.5 text-xs font-medium text-gray-600 dark:text-gray-300"
+                >
+                  Pending <ClockIcon className="h-4 w-4" />
+                </label>
+              </div>
+              <div className="flex items-center">
+                <input
+                  id="paid"
+                  name="status"
+                  type="radio"
+                  value="paid"
+                  defaultChecked={invoice.status === 'paid'}
+                  className="h-4 w-4 border-gray-300 bg-gray-100 text-gray-600 focus:ring-2 focus:ring-gray-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-gray-600"
+                />
+                <label
+                  htmlFor="paid"
+                  className="ml-2 flex items-center gap-1.5 rounded-full bg-green-500 px-3 py-1.5 text-xs font-medium text-white dark:text-gray-300"
+                >
+                  Paid <CheckIcon className="h-4 w-4" />
+                </label>
+              </div>
+            </div>
+          </div>
+        </fieldset>
+        {state.errors?.status ? (
+          <div
+            id="status-error"
+            aria-live="polite"
+            className="mt-2 text-sm text-red-500"
           >
-            Cancel
-          </Link>
-          <Button type="submit">Editar Referencial</Button>
-        </div>
+            {state.errors.status.map((error: string) => (
+              <p key={error}>{error}</p>
+            ))}
+          </div>
+        ) : null}
+        {state.message ? (
+          <div
+            id="message-error"
+            aria-live="polite"
+            className="mt-2 text-sm text-red-500"
+          >
+            <p key={state.message}>{state.message}</p>
+          </div>
+        ) : null}
+      </div>
+      <div className="mt-6 flex justify-end gap-4">
+        <Link
+          href="/dashboard/invoices"
+          className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
+        >
+          Cancel
+        </Link>
+        <Button type="submit">Edit Invoice</Button>
       </div>
     </form>
   );
