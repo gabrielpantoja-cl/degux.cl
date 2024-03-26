a5d20a3409337a4a5d8fa77129c211ec48f31616
fitogabo <gapantoj@uc.cl>
2024-03-25 22:01:38 -0300

    Update customer references to colaborador references in EditInvoiceForm

--- app/ui/referenciales/edit-form.tsx
+++ app/ui/referenciales/edit-form.tsx
@@ -1,7 +1,7 @@
 // app/ui/referenciales/edit-form.tsx
 'use client';
 
-import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
+import { ColaboradorField, ReferencialForm } from '@/app/lib/definitions';
 import {
   CheckIcon,
   ClockIcon,
@@ -15,10 +15,10 @@ import { useFormState } from 'react-dom';
 
 export default function EditInvoiceForm({
   invoice,
-  customers,
+  colaboradores,
 }: {
-  invoice: InvoiceForm;
-  customers: CustomerField[];
+  invoice: ReferencialForm;
+  colaboradores: ColaboradorField[];
 }) {
   // this not necessary, and invoice.id is the argument need
   const initialState = { message: null, errors: {} };
@@ -28,25 +28,25 @@ export default function EditInvoiceForm({
   return (
     <form action={dispatch}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
-        {/* Customer Name */}
+        {/* Colaborador Name */}
         <div className="mb-4">
-          <label htmlFor="customer" className="mb-2 block text-sm font-medium">
-            Choose customer
+          <label htmlFor="colaborador" className="mb-2 block text-sm font-medium">
+            Choose colaborador
           </label>
           <div className="relative">
             <select
-              id="customer"
-              name="customerId"
+              id="colaborador"
+              name="colaboradorId"
               className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-              defaultValue={invoice.customer_id}
-              aria-describedby="customer-error"
+              defaultValue={invoice.colaborador_id}
+              aria-describedby="colaborador-error"
             >
               <option value="" disabled>
-                Select a customer
+                Select a colaborador
               </option>
-              {customers.map((customer) => (
-                <option key={customer.id} value={customer.id}>
-                  {customer.name}
+              {colaboradores.map((colaborador) => (
+                <option key={colaborador.id} value={colaborador.id}>
+                  {colaborador.name}
                 </option>
               ))}
             </select>
@@ -54,7 +54,7 @@ export default function EditInvoiceForm({
           </div>
           {state.errors?.colaboradorId ? (
             <div
-              id="customer-error"
+              id="colaborador-error"
               aria-live="polite"
               className="mt-2 text-sm text-red-500"
             >
@@ -86,7 +86,7 @@ export default function EditInvoiceForm({
           </div>
           {state.errors?.monto ? (
             <div
-              id="customer-error"
+              id="colaborador-error"
               aria-live="polite"
               className="mt-2 text-sm text-red-500"
             >
