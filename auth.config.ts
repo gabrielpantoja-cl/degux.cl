a26fe6126abeb0208d45468051509438fbc49980
fitogabo <gapantoj@uc.cl>
2024-03-30 02:06:11 -0300

    Este código define un manejador de API que utiliza next-auth para manejar las solicitudes de autenticación. El manejador de API se configura para usar la configuración de autenticación que definiste en auth.config.ts

--- auth.config.ts
+++ auth.config.ts
@@ -1,22 +1,38 @@
-// Path: auth.config.ts
 import type { NextAuthConfig } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
 
-export const authConfig = {
-  providers: [],
+export const authConfig: NextAuthConfig = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      }
+    }),
+  ],
   pages: {
     signIn: '/login',
   },
   callbacks: {
-    authorized({ auth, request: { nextUrl } }) {
-      const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
+    async signIn({ account, profile }) {
+      if (account?.provider === "google") {
+        return profile?.email_verified && profile?.email?.endsWith("@app.gabrielpantoja.cl") || false;
       }
       return true;
     },
+    async redirect({ baseUrl }) {
+      return baseUrl;
+    },
+    async session({ session }) {
+      return session;
+    },
+    async jwt({ token }) {
+      return token;
+    }
   },
-} satisfies NextAuthConfig;
+};
--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,15 @@
+import { NextApiResponse, NextApiRequest } from 'next';
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
- 
-export default NextAuth(authConfig).auth;
- 
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  return NextAuth(authConfig);
+}
+
 export const config = {
+  api: {
+    bodyParser: false,
+  },
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
   matcher: ['/((?!api|_next/static|_next/image|customers|.png).*)'],
 };
