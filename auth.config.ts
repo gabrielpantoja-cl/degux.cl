b49c18731d6abb68187daac928a4be1cc33f4f7e
fitogabo <gapantoj@uc.cl>
2024-03-31 12:48:02 -0300

    haciendo rollback para restaurar

--- auth.config.ts
+++ auth.config.ts
@@ -1,38 +1,22 @@
+// Path: auth.config.ts
 import type { NextAuthConfig } from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
 
-export const authConfig: NextAuthConfig = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code"
-        }
-      }
-    }),
-  ],
+export const authConfig = {
+  providers: [],
   pages: {
     signIn: '/login',
   },
   callbacks: {
-    async signIn({ account, profile }) {
-      if (account?.provider === "google") {
-        return profile?.email_verified && profile?.email?.endsWith("@app.gabrielpantoja.cl") || false;
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL('/dashboard', nextUrl));
       }
       return true;
     },
-    async redirect({ baseUrl }) {
-      return baseUrl;
-    },
-    async session({ session }) {
-      return session;
-    },
-    async jwt({ token }) {
-      return token;
-    }
   },
-};
+} satisfies NextAuthConfig;
