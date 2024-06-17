edc41a5462a8470de4cfee1b1862c992c5ffc599
fitogabo <gapantoj@uc.cl>
2024-06-17 02:40:38 -0400

    feat: Update EditReferencialForm component to use Spanish labels and placeholders

--- app/ui/referenciales/edit-form.tsx
+++ app/ui/referenciales/edit-form.tsx
@@ -3,8 +3,6 @@
 
 import { Prisma } from '@prisma/client';
 import {
-  CheckIcon,
-  ClockIcon,
   CurrencyDollarIcon,
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
@@ -27,7 +25,7 @@ export default function EditReferencialForm({
         {/* Colaborador Name */}
         <div className="mb-4">
           <label htmlFor="colaborador" className="mb-2 block text-sm font-medium">
-            Choose colaborador
+            Elige colaborador
           </label>
           <div className="relative">
             <select
@@ -39,7 +37,7 @@ export default function EditReferencialForm({
               <option value="" disabled>
                 Select a colaborador
               </option>
-              {colaboradores.map((colaborador) => (
+              {Array.isArray(colaboradores) && colaboradores.map((colaborador) => (
                 <option key={colaborador.id} value={colaborador.id}>
                   {colaborador.name}
                 </option>
@@ -51,22 +49,20 @@ export default function EditReferencialForm({
 
         {/* Referencial Monto */}
         <div className="mb-4">
-          <label htmlFor="monto" className="mb-2 block text-sm font-medium">
-            Choose an monto
+          <label htmlFor="fojas" className="mb-2 block text-sm font-medium">
+            Fojas
           </label>
           <div className="relative mt-2 rounded-md">
             <div className="relative">
-              <input
-                id="monto"
-                name="monto"
-                type="number"
-                step="0.01"
-                defaultValue={referencial.monto}
-                placeholder="Enter CLP monto"
-                className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-              />
-              <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-            </div>
+            <input
+              id="fojas"
+              name="fojas"
+              type="number"
+              defaultValue={referencial.fojas} // Asumiendo que 'referencial' es el objeto que contiene los datos actuales
+              className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              placeholder="Ingrese el número de fojas"
+  />
+        </div>
           </div>
         </div>
       </div>
