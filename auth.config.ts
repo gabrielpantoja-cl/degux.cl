6e78a7e68a1e44aae7b6f225d09e50273752b37b
fitogabo <gapantoj@uc.cl>
2024-06-17 01:04:06 -0400

    chore: Update basePath handling in auth.config.ts

--- auth.config.ts
+++ auth.config.ts
@@ -1,20 +1,25 @@
 // Path: auth.config.ts
 import type { NextAuthConfig } from 'next-auth';
 
+// Define el basePath solo para el entorno de desarrollo
+const isDev = process.env.NODE_ENV === 'development';
+const basePath = isDev ? '/devBasePath' : '';
+
 export const authConfig = {
   providers: [],
   pages: {
-    signIn: '/login',
+    signIn: `${basePath}/login`,
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      const isOnDashboard = nextUrl.pathname.startsWith(`${basePath}/dashboard`);
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
+        // Asegúrate de incluir el basePath en la URL de redirección
+        return Response.redirect(new URL(`${basePath}/dashboard`, nextUrl));
       }
       return true;
     },
