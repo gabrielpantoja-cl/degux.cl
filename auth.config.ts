9d02021dad9a15c0e9418fff8fc7336a53b44f0f
fitogabo <gapantoj@uc.cl>
2024-06-17 02:26:49 -0400

    chore: Update auth.config.ts to use relative paths for signIn URLs

--- auth.config.ts
+++ auth.config.ts
@@ -2,26 +2,22 @@
 import type { NextAuthConfig } from 'next-auth';
 import { NextResponse } from 'next/server'; // Asegúrate de añadir esta línea
 
-// Define el basePath solo para el entorno de desarrollo
-const isDev = process.env.NODE_ENV === 'development';
-const basePath = isDev ? '/devBasePath' : '';
-
 export const authConfig = {
   providers: [],
   pages: {
-    signIn: `${basePath}/login`,
+    signIn: '/login',
   },
   callbacks: {
     async authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith(`${basePath}/dashboard`);
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         // Redirige a los usuarios no autenticados a la página de inicio de sesión
-        return NextResponse.redirect(new URL(`${basePath}/login`, nextUrl.origin));
+        return NextResponse.redirect(new URL('/login', nextUrl.origin));
       } else if (isLoggedIn) {
         // Redirige a los usuarios autenticados al dashboard
-        return NextResponse.redirect(new URL(`${basePath}/dashboard`, nextUrl.origin));
+        return NextResponse.redirect(new URL('/dashboard', nextUrl.origin));
       }
       return true;
     },
