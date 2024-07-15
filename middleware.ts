1cde496f8c5272f0f383d064bb68ad7de389f311
fitogabo <gapantoj@uc.cl>
2024-07-15 04:20:46 -0400

    Update auth middleware import statement

--- middleware.ts
+++ middleware.ts
@@ -1,32 +1,6 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
-import { getToken } from 'next-auth/jwt'; // Importar el método getToken de next-auth/jwt
+export { auth as middleware } from '@/app/lib/auth';
 
-// Define the middleware function
-export async function middleware(request: NextRequest) {
-  const secret = process.env.AUTH_SECRET;
-  const salt = process.env.AUTH_SALT;
-
-  if (!secret) {
-    throw new Error('AUTH_SECRET no está definido en las variables de entorno');
-  }
-
-  if (!salt) {
-    throw new Error('AUTH_SALT no está definido en las variables de entorno');
-  }
-
-  // Obtener el token de sesión
-  const token = await getToken({ req: request, secret, salt });
-
-  // Verificar si el token existe
-  if (!token) {
-    return NextResponse.redirect(new URL('/auth/signin', request.url));
-  }
-
-  return NextResponse.next();
-}
-
-// Configuración del matcher
+// Don't invoke Middleware on some paths
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
 };
