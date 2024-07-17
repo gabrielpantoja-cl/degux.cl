b392e5df360572f898aaa69116d956d3edce97bf
fitogabo <gapantoj@uc.cl>
2024-07-17 02:25:54 -0400

    feat: Update logout route to properly configure and delete the token cookie
    The code changes in logout/route.ts update the logout route to properly configure and delete the token cookie. The cookie is now set with the appropriate options for improved security, including the 'httpOnly', 'secure', and 'sameSite' attributes. Additionally, the cookie is set to expire immediately to ensure its deletion. This change enhances the logout functionality and ensures that the user's session is properly terminated.
    
    Co-authored-by: [Author Name]

--- app/api/users/logout/route.ts
+++ app/api/users/logout/route.ts
@@ -1,15 +1,24 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-export const GET = async() => {
-    try{
+export const GET = async () => {
+    try {
         const response = NextResponse.json({
             message: 'Logout successfully',
             success: true
         });
-        response.cookies.set('token', '', {httpOnly: true, expires: new Date(0)});
+
+        // Configuración adecuada de la cookie para eliminarla
+        response.cookies.set('token', '', {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'none', 
+            path: '/',
+            expires: new Date(0) // Expira inmediatamente para eliminar la cookie
+        });
+
         return response;
+    } catch (err) {
+        const errorMessage = err instanceof Error ? err.message : "Failed to logout";
+        return NextResponse.json({ error: errorMessage }, { status: 500 });
     }
-    catch(err){
-        return NextResponse.json({error: err ?? "Failed to logout"}, {status: 500});
-    }
-}
+};
