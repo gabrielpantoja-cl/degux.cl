a61592dd63487d965d61c025c140474d2f9e0dce
fitogabo <gapantoj@uc.cl>
2024-07-16 22:20:36 -0400

    .env.vercel

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,22 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export const middleware = (request: NextRequest) => {
-    const path = request?.nextUrl?.pathname;
+    const path = request.nextUrl.pathname;
     const isPublicPath = path === '/login';
     const token = request.cookies.get('token')?.value || request.cookies.get('next-auth.session-token')?.value || '';
 
-    if(isPublicPath && token)
-        return NextResponse.redirect(new URL('/', request.nextUrl));
+    // Si el usuario está autenticado y trata de acceder a /login, redirigir a la página de inicio
+    if (isPublicPath && token) {
+        return NextResponse.redirect(new URL('/', request.url));
+    }
 
-    if(!isPublicPath && !token)
-        return NextResponse.redirect(new URL('/login', request.nextUrl));
+    // Si el usuario no está autenticado y trata de acceder a una página protegida, redirigir a /login
+    if (!isPublicPath && !token) {
+        return NextResponse.redirect(new URL('/login', request.url));
+    }
+
+    // Permitir el acceso a la página solicitada
+    return NextResponse.next();
 }
 
 export const config = {
