6a774b976be890da55c8ae324ac2e5e0c1dc9776
fitogabo <gapantoj@uc.cl>
2024-07-26 23:24:10 -0400

    feat: Agregar estado de carga y manejo de errores al botón de inicio de sesión
    Se agregó el estado de carga y el manejo de errores al botón de inicio de sesión en el formulario de inicio de sesión. Esto mejora la experiencia del usuario al proporcionar retroalimentación visual durante el proceso de inicio de sesión y manejar posibles errores de manera adecuada.

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,13 +1,14 @@
 // app/ui/login-form.tsx
 'use client';
 
+import { useState } from 'react';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
 import { signIn } from 'next-auth/react'; 
 
 interface LoginFormProps {
   isVerified: boolean;
-  OAuthAccountNotLinked: boolean; // Añadir esta línea
+  OAuthAccountNotLinked: boolean;
 }
 
 export default function LoginForm({ isVerified, OAuthAccountNotLinked }: LoginFormProps) {
@@ -21,7 +22,7 @@ export default function LoginForm({ isVerified, OAuthAccountNotLinked }: LoginFo
           <p className="text-red-500">Tu cuenta de Google no está vinculada.</p>
         )}
       </div>
-      <LoginButton isVerified={isVerified} /> {/* Añadir el botón de login con la propiedad isVerified */}
+      <LoginButton isVerified={isVerified} />
     </div>
   );
 }
@@ -31,22 +32,34 @@ interface LoginButtonProps {
 }
 
 function LoginButton({ isVerified }: LoginButtonProps) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
   const handleSignIn = async () => {
+    setLoading(true);
+    setError(null);
     try {
       await signIn("google", { callbackUrl: process.env.NEXT_PUBLIC_CALLBACK_URL });
     } catch (error) {
       console.error('Error al iniciar sesión:', error);
+      setError('Error al iniciar sesión. Por favor, inténtalo de nuevo.');
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
-    <Button 
-      className="mt-4 w-full" 
-      onClick={handleSignIn}
-      aria-label="Log in with Google"
-      disabled={!isVerified} // Deshabilitar el botón si no está verificado
-    >
-      Log in with Google <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
-    </Button>
+    <div>
+      <Button 
+        className="mt-4 w-full" 
+        onClick={handleSignIn}
+        aria-label="Log in with Google"
+        disabled={!isVerified || loading}
+      >
+        {loading ? 'Cargando...' : 'Log in with Google'} 
+        {!loading && <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />}
+      </Button>
+      {error && <p className="mt-2 text-red-500">{error}</p>}
+    </div>
   );
 }
