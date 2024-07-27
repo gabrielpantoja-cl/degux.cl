d14d6b31be8bf6d5710f8c95aa7a0465b11455b6
fitogabo <gapantoj@uc.cl>
2024-07-27 10:56:03 -0400

    Eliminar componente de registro y su importación en form-register.tsx

--- app/(auth)/register/page.tsx
+++
@@ -1,7 +0,0 @@
-// Source:app/(auth)/register/page.tsx
-import FormRegister from "@/components/form-register";
-
-const RegisterPage = () => {
-  return <FormRegister />;
-};
-export default RegisterPage;
