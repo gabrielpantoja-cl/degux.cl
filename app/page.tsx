1cb531289a619e9842da9b138a0e48b4a45540a5
fitogabo <gapantoj@uc.cl>
2024-07-26 22:19:12 -0400

    feat: Actualizar página de inicio con imagen de héroe
    Se eliminó el comentario que indicaba agregar imágenes de héroe en la página de inicio y se agregó una imagen de héroe utilizando la biblioteca Next/Image. Esto mejora la apariencia visual de la página y brinda una mejor experiencia al usuario al cargar la imagen de manera diferida.
    
    Co-authored-by: [Author Name]

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 import Link from 'next/link';
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -13,8 +14,8 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" />
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-          Bienvenido a <strong>referenciales.cl</strong><br />
-          Una base de datos colaborativa para la tasación inmobiliaria en Chile.
+            Bienvenido a <strong>referenciales.cl</strong><br />
+            Una base de datos colaborativa para la tasación inmobiliaria en Chile.
           </p>
           <Link
             href="/login"
@@ -24,7 +25,6 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
           <Image
             src="/hero-desktop.png"
             width={1000}
