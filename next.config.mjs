56965eca7143a42c32137e9b48705b7e54b30c9e
fitogabo <gapantoj@uc.cl>
2024-10-26 23:48:30 -0300

    chore: Habilitar modo estricto de React y minificación con SWC en la configuración de Next.js

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,13 @@ const nextConfig = {
     domains: ['lh3.googleusercontent.com'], // Permite imágenes de Google
   },
   // Añade cualquier otra configuración necesaria aquí
+  reactStrictMode: true, // Habilita el modo estricto de React
+  swcMinify: true, // Habilita la minificación con SWC
+  env: {
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+  },
 };
 
 export default nextConfig;
