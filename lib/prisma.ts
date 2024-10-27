18417c75081d479197f19f2e2a2c27a88fddeb11
fitogabo <gapantoj@uc.cl>
2024-10-27 02:52:42 -0300

    chore: Eliminar importación de noStore en prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,8 +3,7 @@
 'use server';
 
 import { PrismaClient } from '@prisma/client';
-import { unstable_noStore as noStore } from 'next/cache';
 
 const prisma = new PrismaClient();
 
-export { prisma, noStore };
+export { prisma };
