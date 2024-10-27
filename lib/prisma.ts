d7c6e5d5bb162664909e2a254ab9793cb8a4ae3f
fitogabo <gapantoj@uc.cl>
2024-10-27 03:05:01 -0300

    chore: Implementar instancia única de PrismaClient en prisma.ts y ajustar la inicialización global

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,11 @@
 // app/lib/prisma.ts
 
-'use server';
-
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 export { prisma };
