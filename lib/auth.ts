6fc2ba5736f2a1b209bd4b04404641ff17023d58
fitogabo <gapantoj@uc.cl>
2024-10-27 06:00:00 -0300

    chore: Extender el tipo Session para incluir id y role del usuario en el token JWT y mejorar la gestión de sesiones

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,18 @@
-import { AuthOptions, Session, User } from 'next-auth';
+import { AuthOptions, Session } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { prisma } from '@/lib/prisma';
+import { JWT } from 'next-auth/jwt';
+
+// Extender el tipo Session para incluir la propiedad id
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      id: string;
+      role: string;
+    } & DefaultSession['user'];
+  }
+}
 
 const googleClientId = process.env.GOOGLE_CLIENT_ID;
 const googleClientSecret = process.env.GOOGLE_CLIENT_SECRET;
@@ -26,15 +37,28 @@ const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   session: { strategy: "database" as const },
   callbacks: {
-    async session({ session, user }: { session: Session; user: User }) {
+    async session({ session, token }: { session: Session; token: JWT }) {
       if (session.user) {
-        session.user.role = user.role;
+        session.user.id = token.id as string;
+        session.user.role = token.role as string;
       }
       return session;
     },
     async redirect({ url, baseUrl }) {
-      console.log('Redirecting to:', url.startsWith(baseUrl) ? url : `${baseUrl}/dashboard`);
+      // Redirigir al dashboard después de la autenticación
       return url.startsWith(baseUrl) ? url : `${baseUrl}/dashboard`;
+    },
+    async signIn(/*{ user, account, profile, email, credentials }*/) {
+      // Aquí puedes agregar lógica adicional para manejar el inicio de sesión
+      return true;
+    },
+    async jwt({ token, user /*, account, profile, isNewUser*/ }) {
+      // Agregar información adicional al token JWT
+      if (user) {
+        token.id = user.id;
+        token.role = user.role;
+      }
+      return token;
     },
   },
   pages: {
