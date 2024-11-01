5d4df00cc861ee4d68808d2e5be29a503a5f16eb
fitogabo <gapantoj@uc.cl>
2024-11-01 03:07:16 -0300

    feat: Refactor autenticación con NextAuth y simplificar configuración de Google Provider

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,61 +1,22 @@
-import { AuthOptions, Session, User } from 'next-auth';
+import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import { prisma } from '@/lib/prisma';
 
 const googleClientId = process.env.GOOGLE_CLIENT_ID;
 const googleClientSecret = process.env.GOOGLE_CLIENT_SECRET;
-const googleRedirectUri = process.env.GOOGLE_REDIRECT_URI;
 
-if (!googleClientId || !googleClientSecret || !googleRedirectUri) {
-  throw new Error("Missing Google client ID, secret, or redirect URI in environment variables");
+if (!googleClientId || !googleClientSecret) {
+  throw new Error("Missing Google client ID or secret in environment variables");
 }
 
-const authOptions: AuthOptions = {
+export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     GoogleProvider({
       clientId: googleClientId,
       clientSecret: googleClientSecret,
-      authorization: {
-        params: {
-          redirect_uri: googleRedirectUri,
-        },
-      },
     }),
   ],
-  adapter: PrismaAdapter(prisma),
-  session: {
-    strategy: "database" as const,
-  },
-  cookies: {
-    sessionToken: {
-      name: `__Secure-next-auth.session-token`,
-      options: {
-        httpOnly: true,
-        sameSite: "none",
-        path: "/",
-        secure: true,
-      },
-    },
-    csrfToken: {
-      name: `__Host-next-auth.csrf-token`,
-      options: {
-        httpOnly: true,
-        sameSite: "none",
-        path: "/",
-        secure: true,
-      },
-    },
-  },
   callbacks: {
-    async session({ session, user }: { session: Session; user: User }) {
-      if (session.user) {
-        session.user.role = user.role;
-      }
-      return session;
-    },
     async redirect({ url, baseUrl }) {
-      // Asegúrate de redirigir al dashboard después de la autenticación
       return url.startsWith(baseUrl) ? url : `${baseUrl}/dashboard`;
     },
   },
@@ -65,6 +26,4 @@ const authOptions: AuthOptions = {
   },
   secret: process.env.NEXTAUTH_SECRET,
   debug: process.env.NEXTAUTH_DEBUG === 'true',
-};
-
-export { authOptions };
+});
