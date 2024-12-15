93b281f7d9cb445f0bcc3c72014f4ca956f2ef54
fitogabo <gapantoj@uc.cl>
2024-12-15 20:16:12 -0300

    feat: Renombrar el campo "user_id" a "userId" en la función de obtención de referenciales

--- lib/mapData.ts
+++ lib/mapData.ts
@@ -23,7 +23,7 @@ export async function fetchReferencialesForMap() {
         superficie, 
         monto, 
         observaciones, 
-        user_id as "userId"
+        userId
       FROM referenciales 
       WHERE lat IS NOT NULL AND lng IS NOT NULL
     `;
