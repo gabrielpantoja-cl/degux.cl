08a32a3d8582f6e216cd79fd2fa59bccc616e7a9
fitogabo <gapantoj@uc.cl>
2024-12-16 03:34:30 -0300

    feat: Actualizar el placeholder y la validación del campo 'fojas' para incluir nuevos formatos

--- components/ui/referenciales/FormFields.tsx
+++ components/ui/referenciales/FormFields.tsx
@@ -40,11 +40,11 @@ const FormFields: React.FC<FormFieldsProps> = ({ state, currentUser }) => (
         name="fojas"
         id="fojas"
         className={`mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm ${
-          state.invalidFields.has('fojas') ? 'border-red-500' : ''
+        state.invalidFields.has('fojas') ? 'border-red-500' : ''
         }`}
-        placeholder="Ejemplos: 157v, 1586 v, 1838 vuelta"
-        pattern="^[0-9]+(\s?[vV](?:uelta)?)?$"
-        title="Ingrese un número seguido opcionalmente de 'v', 'V' o 'vuelta'"
+        placeholder="Ejemplos: 157v, 1586 v, 1838 vuelta, 123 vta"  
+        pattern="^[0-9]+(\s?([vV](?:uelta)?|[vV]ta)?)?$"
+        title="Ingrese un número seguido opcionalmente de 'v', 'V', 'vta' o 'vuelta' (con o sin espacio)"
         required
       />
       {state.errors.fojas && (
