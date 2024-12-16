8ec5e0edc887ff3c33f27b39ff67fc94d1b4c184
fitogabo <gapantoj@uc.cl>
2024-12-15 23:01:52 -0300

    feat: Ajustar el formato del campo "monto" en la función de obtención de referenciales

--- lib/mapData.ts
+++ lib/mapData.ts
@@ -21,7 +21,7 @@ export async function fetchReferencialesForMap() {
         rol, 
         fechaescritura, 
         superficie, 
-        monto, 
+        monto,      
         observaciones, 
         userId
       FROM referenciales 
