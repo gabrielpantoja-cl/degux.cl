5743c5d144c7dc5859f65b5dd38b4b8f143f2545
fitogabo <gapantoj@uc.cl>
2025-01-03 02:24:44 -0300

    refactor: corregir rutas de archivos de pruebas y mejorar configuración de Jest

--- __tests__/config/jest.config.mjs
+++ __tests__/config/jest.config.mjs
@@ -1,4 +1,4 @@
-// __tests__/config/jest.config.mjs
+// jest.config.mjs
 import nextJest from 'next/jest';
 
 const createJestConfig = nextJest({
@@ -9,17 +9,30 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
+  moduleDirectories: ['node_modules', '<rootDir>'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
   },
+  testMatch: [
+    "**/__tests__/**/*.[jt]s?(x)",
+    "**/__tests__/app/(*)/**/*.[jt]s?(x)",
+    "**/?(*.)+(spec|test).[jt]s?(x)"
+  ],
   testPathIgnorePatterns: [
     '<rootDir>/.next/',
     '<rootDir>/node_modules/',
     '<rootDir>/coverage/',
     '<rootDir>/dist/',
   ],
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx|mjs)$': ['babel-jest', { presets: ['next/babel'] }]
+  },
   coveragePathIgnorePatterns: [
     '/node_modules/',
     'jest.config.js',
@@ -30,6 +43,7 @@ const customJestConfig = {
   collectCoverage: true,
   collectCoverageFrom: [
     'src/**/*.{js,jsx,ts,tsx}',
+    'app/**/*.{js,jsx,ts,tsx}',
     '!**/*.d.ts',
     '!**/node_modules/**',
   ],
