3f26881c55921da7398209afbcf8439e9411f3a7
fitogabo <gapantoj@uc.cl>
2025-01-03 00:44:30 -0300

    refactor: mejorar la obtención del token en la ruta de verificación de correo electrónico

--- app/api/auth/verify-email/route.ts
+++ app/api/auth/verify-email/route.ts
@@ -5,8 +5,8 @@ import { type NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
   try {
-    const searchParams = request.nextUrl.searchParams;
-    const token = searchParams.get("token");
+    const url = new URL(request.url);
+    const token = url.searchParams.get("token");
 
     if (!token) {
       return new Response("Token no encontrado", { status: 400 });
