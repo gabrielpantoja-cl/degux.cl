b2ca53a7530b062ba1748f683d7c32505bf77ba3
fitogabo <gapantoj@uc.cl>
2025-01-04 19:26:16 -0300

    refactor: actualizar .gitignore y agregar constantes de prueba en helpers

---
+++ __tests__/__helpers__/constants.js
@@ -0,0 +1,10 @@
+// __tests__/__helpers__/constants.js
+export const ROUTES = {
+  HOME: 'http://localhost:3000',
+  LOGIN: '/login'
+};
+
+export const TEST_IDS = {
+  LOADING_SPINNER: 'loading-spinner'
+};
+
