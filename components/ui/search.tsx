2edf8163426a5323285a71bb586aac722e6f9365
fitogabo <gapantoj@uc.cl>
2025-01-09 16:39:37 -0300

    refactor: mejorar la gestión de parámetros de búsqueda en el componente de búsqueda

--- components/ui/search.tsx
+++ components/ui/search.tsx
@@ -10,16 +10,20 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
 
   const handleSearch = useDebouncedCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    const params = new URLSearchParams(searchParams)
-    const term: string = e.target.value
+    const params = new URLSearchParams(searchParams?.toString() ?? '');
+    const term: string = e.target.value;
     params.set('page', '1');
+    
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
+    
     replace(`${pathname}?${params.toString()}`);
-  }, 300)
+  }, 300);
+
+  const defaultSearchQuery = searchParams?.get('query')?.toString() ?? '';
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -30,7 +34,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
         onChange={handleSearch}
-        defaultValue={searchParams.get('query')?.toString()}
+        defaultValue={defaultSearchQuery}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
